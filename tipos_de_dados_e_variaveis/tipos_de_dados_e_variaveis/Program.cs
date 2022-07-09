4f6220985c17a3a0c6245b871bb2cc2d411ffd82
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-09 20:19:40 -0300

    tipos de dados e variáveis

---
+++ tipos_de_dados_e_variaveis/tipos_de_dados_e_variaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace tipos_de_dados_e_variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 20;
+            salario = 5800.5;
+            altura = 1.63;
+            genero = 'F';
+            nome = "Maria Silva";
+
+            Console.WriteLine(idade);
+            Console.WriteLine(salario);
+            Console.WriteLine(altura);
+            Console.WriteLine(genero);
+            Console.WriteLine(nome);
+        }
+    }
+}
