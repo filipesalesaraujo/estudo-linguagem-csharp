124d55f1fca0a608fd01eee14b3b670d2d035bed
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-09 20:29:32 -0300

    saída de dados

---
+++ saida-de-dados/saida-de-dados/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace saida_de_dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bom dia");
+            Console.Write("Boa noite");
+
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Boa noite");
+
+            int x, y;
+            x = 10;
+            y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double x2;
+            x2 = 2.3456;
+            Console.Write(x2.ToString("F2", CI));
+            Console.WriteLine();
+
+            int idade;
+            double salario;
+            string nome;
+            char sexo;
+            idade = 32;
+            salario = 4560.9;
+            nome = "Maria Silva";
+            sexo = 'F';
+            Console.WriteLine("A funcionaria " + nome + ", sexo " + sexo + ", ganha " + salario.ToString("F2", CI) + " e tem " + idade + " anos");
+        }
+    }
+}
