92438d42eb2c2eabbbf414cfd507c5d904000ab0
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-09 20:40:54 -0300

    processamento de dados e casting

---
+++ processamento_de_dados_e_casting/processamento_de_dados_e_casting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace processamento_de_dados_e_casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 5;
+            y = 2 * x;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int x2;
+            double y2;
+            x2 = 5;
+            y2 = 2 * x2;
+            Console.WriteLine(x2);
+            Console.WriteLine(y2.ToString("F1", CI));
+
+            double b1, b2, h, area;
+            b1 = 6.0;
+            b2 = 8.0;
+            h = 5.0;
+            area = (b1 + b2) / 2.0 * h;
+            Console.WriteLine(area);
+
+            int a, b, resultado;
+            a = 5;
+            b = 2;
+            resultado = a / b;
+            Console.WriteLine(resultado);
+
+            double a3;
+            int b3;
+            a3 = 5.0;
+            b3 = (int)a3;
+            Console.WriteLine(b3);
+        }
+    }
+}
