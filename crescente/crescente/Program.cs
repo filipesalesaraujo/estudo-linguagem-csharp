d145dce284b92f95fb9b4985009d00d99b984c15
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-09 22:04:24 -0300

    crescente

---
+++ crescente/crescente/Program.cs
@@ -0,0 +1,40 @@
+﻿//Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma
+//mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente. O
+//programa deve finalizar quando forem digitados dois valores iguais.
+
+using System.Globalization;
+
+namespace crescente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int x, y;
+
+            Console.WriteLine("Digite dois numeros:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                else
+                {
+                    Console.WriteLine("DECRESCEBTE!");
+                }
+                Console.WriteLine("Digite outros dois numeros:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
