6926e796cf62f5e394baca460c87460237a715ee
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-09 21:59:08 -0300

    menor de tres

---
+++ menor_de_tres/menor_de_tres/Program.cs
@@ -0,0 +1,43 @@
+﻿//Fazer um programa para ler três números inteiros. Em seguida, mostrar qual o menor dentre os três
+//números lidos. Em caso de empate, mostrar apenas uma vez.
+
+using System.Globalization;
+
+namespace menor_de_tres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int a, b, c, menor;
+
+            Console.Write("Primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                menor = a;
+            }
+            else if (b < c)
+            {
+                menor = b;
+            }
+            else
+            {
+                menor = c;
+            }
+
+            Console.WriteLine("MENOR = " + menor);
+
+        }
+    }
+}
